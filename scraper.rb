35883646f89d6a65f6967df8112b3ae350b7b2d2
nuxie <zkalinowska96@icloud.com>
2020-02-05 14:38:32 +0100

    Revert "Added Hash with all the job details scraped"
    This reverts commit 16e40eb065e2adc91ad2572486ef410a8e8a2adc.

--- scraper.rb
+++ scraper.rb
@@ -49,14 +49,7 @@ def justjoin_scraper
     end
     jobs = Array.new
     offers.each do |job_offer|
-        job = {
-            title: job_offer.span(class: 'title').text,
-            location: job_offer.span(class: 'company-address').text.split.last,
-            company: job_offer.span(class: 'company-name').text[2..-1],
-            salary: job_offer.span(class: 'salary-row').text,
-            tags: tags_arr.join(', '),
-            url: job_offer.a.href
-        }
+        job = title: job_offer.span(class: 'title').text
         jobs << job if job[:title] != "" && job_offer.element(css: 'span.age').text.chr.to_i < age_days
     end
     return jobs
