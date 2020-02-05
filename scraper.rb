bbcb9250acf02cbf776a41c977dfca7a2b2e71af
nuxie <zkalinowska96@icloud.com>
2020-02-05 14:25:01 +0100

    Hash with all the jobs details from the 1st page

--- scraper.rb
+++ scraper.rb
@@ -8,7 +8,14 @@ def bulldog_scraper
     parsed_page = Nokogiri::HTML(unparsed_page)
     job_offers = parsed_page.css('ul.results-list li')
     job_offers.each do |job_offer|
-        job = job_offer.css('h2.result-header').text.strip
+        job = {
+            title: job_offer.css('h2.result-header').text.strip,
+            location: job_offer.css('div.result-desc p.result-desc-meta span.pop-mobile').text.strip,
+            company: job_offer.css('div.result-desc p.result-desc-meta span.pop-black').text.strip,
+            salary: job_offer.css('div.result-desc p.result-desc-meta span.pop-green').text.strip,
+            tags: job_offer.css('div.result-desc li.tags-item').text.split(/\n+/).join(" "),
+            url: job_offer.css('a')[0].attributes['href'].value
+        }
     end
 end
 
