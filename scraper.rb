9588d649ee3f08152a5ae362957d8fe9e3208a02
nuxie <zkalinowska96@icloud.com>
2020-02-05 14:31:51 +0100

    Fixed duplicate loop in bulldog_scraper

--- scraper.rb
+++ scraper.rb
@@ -19,20 +19,19 @@ def bulldog_scraper
         pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
         pagination_job_offers = pagination_parsed_page.css('ul.results-list li.results-list-item')
         pagination_job_offers.each do |job_offer|
-            job_offers.each do |job_offer|
-                job = {
-                    title: job_offer.css('h2.result-header').text.strip,
-                    location: job_offer.css('div.result-desc p.result-desc-meta span.pop-mobile').text.strip,
-                    company: job_offer.css('div.result-desc p.result-desc-meta span.pop-black').text.strip,
-                    salary: job_offer.css('div.result-desc p.result-desc-meta span.pop-green').text.strip,
-                    tags: job_offer.css('div.result-desc li.tags-item').text.split(/\n+/).join(" "),
-                    url: job_offer.css('a')[0].attributes['href'].value
-                }
-                jobs << job if job[:title] != ""
-            end
-            page += 1
+            job = {
+                title: job_offer.css('h2.result-header').text.strip,
+                location: job_offer.css('div.result-desc p.result-desc-meta span.pop-mobile').text.strip,
+                company: job_offer.css('div.result-desc p.result-desc-meta span.pop-black').text.strip,
+                salary: job_offer.css('div.result-desc p.result-desc-meta span.pop-green').text.strip,
+                tags: job_offer.css('div.result-desc li.tags-item').text.split(/\n+/).join(" "),
+                url: job_offer.css('a')[0].attributes['href'].value
+            }
+            jobs << job if job[:title] != ""
         end
-        return jobs
+        page += 1
     end
+    return jobs
+end
 
-    bulldog_scraper
+bulldog_scraper
