b930b4a4d0b53fc150c21c3fd7e55ec12a7f226b
nuxie <zkalinowska96@icloud.com>
2020-02-05 14:27:12 +0100

    Implemented scraping all the pages

--- scraper.rb
+++ scraper.rb
@@ -8,19 +8,29 @@ def bulldog_scraper
     unparsed_page = conn.get url
     parsed_page = Nokogiri::HTML(unparsed_page)
     job_offers = parsed_page.css('ul.results-list li')
+    page = 1
+    last_page = parsed_page.css('nav.text-center ul.pagination').text.split(' ').pop.to_i 
     jobs = Array.new
-    job_offers.each do |job_offer|
-        job = {
-            title: job_offer.css('h2.result-header').text.strip,
-            location: job_offer.css('div.result-desc p.result-desc-meta span.pop-mobile').text.strip,
-            company: job_offer.css('div.result-desc p.result-desc-meta span.pop-black').text.strip,
-            salary: job_offer.css('div.result-desc p.result-desc-meta span.pop-green').text.strip,
-            tags: job_offer.css('div.result-desc li.tags-item').text.split(/\n+/).join(", "),
-            url: job_offer.css('a')[0].attributes['href'].value
-        }
-        jobs << job if job[:title] != ""
+    while page <= last_page
+        pagination_url = "https://bulldogjob.pl/companies/jobs?page=#{page}"
+        pagination_unparsed_page = conn.get pagination_url
+        pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page.body)
+        pagination_job_offers = pagination_parsed_page.css('ul.results-list li.results-list-item')
+        pagination_job_offers.each do |job_offer|
+            job_offers.each do |job_offer|
+                job = {
+                    title: job_offer.css('h2.result-header').text.strip,
+                    location: job_offer.css('div.result-desc p.result-desc-meta span.pop-mobile').text.strip,
+                    company: job_offer.css('div.result-desc p.result-desc-meta span.pop-black').text.strip,
+                    salary: job_offer.css('div.result-desc p.result-desc-meta span.pop-green').text.strip,
+                    tags: job_offer.css('div.result-desc li.tags-item').text.split(/\n+/).join(" "),
+                    url: job_offer.css('a')[0].attributes['href'].value
+                }
+                jobs << job if job[:title] != ""
+            end
+            page += 1
+        end
+        return jobs
     end
-    return jobs
-end
 
-bulldog_scraper
+    bulldog_scraper
