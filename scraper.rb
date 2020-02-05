f8fbb6cf019dcb2ddce72859287b79449e23f5fb
nuxie <zkalinowska96@icloud.com>
2020-02-05 14:17:31 +0100

    Scraping job titles from first page

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,9 @@ def bulldog_scraper
     url = "https://bulldogjob.pl/companies/jobs"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+    job_offers = parsed_page.css('ul.results-list li')
+    job_offers.each do |job_offer|
+        job = job_offer.css('h2.result-header').text.strip
     end
 end
 
